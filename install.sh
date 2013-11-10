20f7683e3fc85acffd3876e9390a3e235dceebf1
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-11-10 11:44:54 +1000

    Backing up rc files.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+NO_COLOR="\033[1;0m"
+GREEN="\033[0;32m"
+GRAY="\033[1;30m"
+
+echo -ne "\n${GREEN}Initializing...\n\n"
+
+echo -e "Creating backup + creating symlinks to new dotfiles..."
+echo -e "${GRAY}"
+cd ~/.dotfiles/files; for file in *; do
+  echo "~/.$file"
+  if [ -s ~/.$file ]; then mv ~/.$file ~/.$file.bkp; fi
+  ln -s ~/.dotfiles/files/$file ~/.$file
+done
+
+echo "source \$HOME/.dotfiles/files/bash_profile" >> ~/.zshrc
+echo "source \$HOME/.dotfiles/keysrc" >> ~/.zshrc
+
+
+echo -ne "\n${GREEN}Done! :-)${NO_COLOR}\n\n"
