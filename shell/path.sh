4ae43405436d9d32ca03cba9f3bd51c943f87aaa
Ricardo Bernardeli <ricardobcs@gmail.com>
2017-04-26 11:54:14 +1000

    shell: Add plain docker services to PATH

--- shell/path.sh
+++ shell/path.sh
@@ -14,8 +14,9 @@ append_path "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 append_path "/usr/local/opt/go/libexec/bin"
 append_path "/usr/local/opt/coreutils/libexec/gnubin"
 
-# TODO: Revisit this.
+# EDH path - Revisit this to get rid of /Users/ric
 prepend_path "/Users/ric/Code/plain-utils/bin"
+prepend_path "/Users/ric/Code/plain-services-docker/bin"
 
 unset prepend_path
 unset append_path
