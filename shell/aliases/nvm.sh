3cdc2e0b0a858e8b5e1eb7d007f331c0a4e49226
Ricardo Bernardeli <ricardobcs@gmail.com>
2017-08-04 13:37:21 +1000

    nvm: replace scripts

--- shell/aliases/nvm.sh
+++ shell/aliases/nvm.sh
@@ -1,22 +1,3 @@
-# I want nvm not to fuck with my shell boot time
-nvm() {
-  unset -f nvm
-  export NVM_DIR="$HOME/.nvm"
-  . "$(brew --prefix nvm)/nvm.sh"
-  nvm "$@"
-}
-
-node() {
-  unset -f node
-  export NVM_DIR="$HOME/.nvm"
-  . "$(brew --prefix nvm)/nvm.sh"
-  node "$@"
-}
-
-npm() {
-  unset -f npm
-  export NVM_DIR="$HOME/.nvm"
-  . "$(brew --prefix nvm)/nvm.sh"
-  npm "$@"
-}
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
