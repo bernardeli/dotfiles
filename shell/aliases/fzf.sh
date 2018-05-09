914f9ac9b65d8f4af0a6ebdb81b44b9d57d7c626
Ricardo Bernardeli <ricardobcs@gmail.com>
2018-05-09 15:55:57 +1000

    fzf: set options to fzf

---
+++ shell/aliases/fzf.sh
@@ -0,0 +1,8 @@
+# nix-env -i fzf
+# nix-env -i fd
+
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
