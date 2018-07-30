7113f0f2029a16dd1729853142d6f5205da63e21
Ricardo Bernardeli <ricardobcs@gmail.com>
2018-07-30 10:26:07 +1000

    shell: `bk` to open buildkite with the right branch and project

---
+++ shell/buildkite.sh
@@ -0,0 +1,10 @@
+function bk() {
+  project=$1
+  guessedProjectName=$(basename $(git remote show -n origin | grep Fetch | cut -d. -f2))
+  project=${project:-$guessedProjectName}
+
+  branch=$2
+  branch=${branch:-$(git rev-parse --abbrev-ref HEAD)}
+  branch=${branch:-master}
+  open "https://buildkite.com/fivegoodfriends/$project/builds?branch=$branch"
+}
