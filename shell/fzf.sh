f0cc2bea4a2488ac593a3f525d07576233aa4cfc
Ricardo Bernardeli <ricardobcs@gmail.com>
2018-07-30 16:46:33 +1000

    shell: trying new stuff with fzf

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -6,3 +6,20 @@ export FZF_DEFAULT_COMMAND='fd --type f'
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+fgst() {
+  eval "command git status -s" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" fzf -m "$@" | while read -r item; do
+    printf "$item" | cut -c4-
+  done
+  echo
+}
+
+fvim() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && vim "${files[@]}"
+}
+
+fvg() {
+  vim $(fgst)
+}
