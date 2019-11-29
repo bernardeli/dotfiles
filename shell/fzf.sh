1b92bf6e9d5de112d3f31a2c6347faa2f3790e66
Ricardo Bernardeli <ricardobcs@gmail.com>
2019-11-29 10:34:59 +1000

    FZF aliases

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -30,3 +30,21 @@ frg() {
 
   rspec $(printf $files | paste -s -d " ")
 }
+
+fbr() {
+  local branches branch
+  branches=$(git --no-pager branch -vv) &&
+  branch=$(echo "$branches" | fzf-tmux +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
